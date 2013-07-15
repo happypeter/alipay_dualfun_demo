4f2b8d1a68cfb8f36751456e82ba27aee0b36b9e
Peter Wang <happypeter1983@gmail.com>
2013-07-15 13:04:36 +0800

    rails new alipay_dualfun_demo -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlipayDualfunDemo::Application.config.secret_token = '12cf9df52705851df1e601b2e014b1d3e4bfea4a90ccbdc988a56092823c2d6c124014cf47822ef2834d3c44f48dac22e25cd1e4f32dbf15fd1c48ee54509588'
