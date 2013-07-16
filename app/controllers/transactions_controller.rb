1d92607339e56027110c27bd58891a02456e69bf
Peter Wang <happypeter1983@gmail.com>
2013-07-16 10:15:12 +0800

    stop async notification with `success`

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -18,7 +18,7 @@ class TransactionsController < ApplicationController
   def notify
     notification = Notification.new(total_fee: params[:total_fee], out_trade_no: params[:out_trade_no], notify_time: params[:notify_time])
     notification.save!
-    render text: 'no-success'
+    render text: 'success'
   end
 
   def checkout
