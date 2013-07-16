9aebe97ae33d1dc8e61b0813c53a8ac5959f195d
Peter Wang <happypeter1983@gmail.com>
2013-07-16 09:55:46 +0800

    wrong route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AlipayDualfunDemo::Application.routes.draw do
   get 'transactions/done'
-  get 'transactions/notify'
+  post 'transactions/notify'
   post '/checkout' => "transactions#checkout"
   root :to => 'welcome#index'
 end
